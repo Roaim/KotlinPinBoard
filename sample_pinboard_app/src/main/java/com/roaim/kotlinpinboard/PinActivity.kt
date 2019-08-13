1e026df16aa06a54d7e19a61c804df510a83c382
Roaim Ahmed Hridoy <rah0024hr@gmail.com>
2019-08-13 16:20:09 +0600

    fix fab button crash issue

--- sample_pinboard_app/src/main/java/com/roaim/kotlinpinboard/PinActivity.kt
+++ sample_pinboard_app/src/main/java/com/roaim/kotlinpinboard/PinActivity.kt
@@ -2,7 +2,6 @@ package com.roaim.kotlinpinboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_pin_board.*
 
@@ -12,16 +11,20 @@ class PinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pin_board)
         setSupportActionBar(toolbar)
-        fab.setOnClickListener(Navigation.createNavigateOnClickListener(findNavController(R.id.fragmentNavHost).run {
-            when (currentDestination?.id) {
-                R.id.pinBoardFragment2 -> R.id.action_pinBoardFragment2_to_testActivity2
-                R.id.pinDetailsFragment -> R.id.action_pinDetailsFragment2_to_testActivity2
-                else -> {
-                    R.id.action_pinBoardFragment2_to_testActivity2
-                }
-            }
-        }))
 
+        fab.setOnClickListener {
+            findNavController(R.id.fragmentNavHost).apply {
+                navigate(
+                    when (currentDestination?.id) {
+                        R.id.pinBoardFragment2 -> R.id.action_pinBoardFragment2_to_testActivity2
+                        R.id.pinDetailsFragment -> R.id.action_pinDetailsFragment2_to_testActivity2
+                        else -> {
+                            R.id.action_pinBoardFragment2_to_testActivity2
+                        }
+                    }
+                )
+            }
+        }
     }
 
 }
