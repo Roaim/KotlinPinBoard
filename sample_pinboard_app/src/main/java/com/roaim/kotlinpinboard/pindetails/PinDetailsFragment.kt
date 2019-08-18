69b78644252e1ad18972f3e776ea4cc8eac44bd3
Roaim Ahmed Hridoy <rah0024hr@gmail.com>
2019-08-19 03:29:15 +0600

    add shared element transition

--- sample_pinboard_app/src/main/java/com/roaim/kotlinpinboard/pindetails/PinDetailsFragment.kt
+++ sample_pinboard_app/src/main/java/com/roaim/kotlinpinboard/pindetails/PinDetailsFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.transition.TransitionInflater
 import com.roaim.kotlinpinboard.data.PinRepository
 import com.roaim.kotlinpinboard.databinding.PinDetailsFragmentBinding
 import com.roaim.kotlinpinboard.utils.PinViewModelFactory
 
 class PinDetailsFragment : Fragment() {
+
+    private lateinit var binding: PinDetailsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,13 +23,25 @@ class PinDetailsFragment : Fragment() {
         val viewModel by viewModels<PinDetailsViewModel> { PinViewModelFactory(PinRepository()) }
         return PinDetailsFragmentBinding.inflate(inflater, container, false).also {
             it.viewmodel = viewModel
+            binding = it
         }.run {
             lifecycleOwner = this@PinDetailsFragment
             val safeArg = PinDetailsFragmentArgs.fromBundle(arguments!!)
             viewModel.setTitle(safeArg.title ?: "Something wrong!")
             viewModel.downloadImage(safeArg.imgUrl)
+            viewModel.downloadImage(safeArg.thumbUrl)
             root
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ViewCompat.setTransitionName(binding.frameLayout , "rah")
     }
 
 }
